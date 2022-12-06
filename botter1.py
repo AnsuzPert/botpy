5bf5ab9df9809bc133dd2198e500e6b6a78a0a10
unknown <ansuzpert88@gmail.com>
2022-12-06 14:57:59 +0300

    ботик

---
+++ botter1.py
@@ -0,0 +1,58 @@
+# botter1
+# Te_sto_vibot (t.me/Te_sto_vibot )
+# токен для доступа к HTTP API:
+# 5859318205:AAHlgYQnJ2nFdSS2vrNhHxDeYN19l3Vq3BM
+import telebot;
+from telebot import types
+bot = telebot.TeleBot('5859318205:AAHlgYQnJ2nFdSS2vrNhHxDeYN19l3Vq3BM');
+
+        
+name = '';
+surname = '';
+age = 0;
+@bot.message_handler(content_types=['text'])
+def start(message):
+    if message.text == '/reg':
+        bot.send_message(message.from_user.id, "Как тебя зовут?");
+        bot.register_next_step_handler(message, get_name); #следующий шаг – функция get_name
+    else:
+        bot.send_message(message.from_user.id, 'Напиши /reg');
+
+def get_name(message): #получаем фамилию
+    global name;
+    name = message.text;
+    bot.send_message(message.from_user.id, 'Какая у тебя фамилия?');
+    bot.register_next_step_handler(message, get_surname);
+
+def get_surname(message):
+    global surname;
+    surname = message.text;
+    bot.send_message(message.from_user.id,'Сколько тебе лет?');
+    bot.register_next_step_handler(message, get_age);
+
+def get_age(message):
+    global age;
+    while age == 0: #проверяем что возраст изменился
+        try:
+             age = int(message.text) #проверяем, что возраст введен корректно
+        except Exception:
+             bot.send_message(message.from_user.id, 'Цифрами, пожалуйста');
+             
+    keyboard = types.InlineKeyboardMarkup(); #наша клавиатура
+    key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes'); #кнопка «Да»
+    keyboard.add(key_yes); #добавляем кнопку в клавиатуру
+    key_no= types.InlineKeyboardButton(text='Нет', callback_data='no');
+    keyboard.add(key_no);
+    question = 'Тебе '+str(age)+' лет, тебя зовут '+name+' '+surname+'?';   
+    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+    
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "yes" and age>12: #call.data это callback_data, которую мы указали при объявлении кнопки
+                                
+        bot.send_message(call.message.chat.id, 'Запомню : )');
+    else: 
+        bot.send_message(call.message.chat.id, 'Иди делай домашку');
+
+    
+bot.polling(none_stop=True, interval=0)
